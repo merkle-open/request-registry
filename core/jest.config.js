9cc5ea8e225ffa7aa9945db133ddfb8668722c1f
Jan Nicklas <j.nicklas@me.com>
2019-05-13 08:48:12 +0200

    test(request-registry-react): Add tests

---
+++ core/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  transform: {
+    ".(ts|tsx)":
+      "/Users/jnicklas/Desktop/request-registry/core/node_modules/tsdx/node_modules/ts-jest/dist/index.js"
+  },
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  testMatch: ["<rootDir>/test/**/*.(spec|test).{ts,tsx}"],
+  testURL: "http://localhost",
+  rootDir: "/Users/jnicklas/Desktop/request-registry/core",
+  watchPlugins: [
+    "/Users/jnicklas/Desktop/request-registry/core/node_modules/tsdx/node_modules/jest-watch-typeahead/filename.js",
+    "/Users/jnicklas/Desktop/request-registry/core/node_modules/tsdx/node_modules/jest-watch-typeahead/testname.js"
+  ]
+};
---
+++ utils/request-registry-react/test/index.test.tsx
@@ -0,0 +1,103 @@
+import * as React from "react";
+import { useGetEndPoint } from "../src";
+import { createGetEndpoint } from "request-registry";
+import {
+  mockEndpoint,
+  unmockAllEndpoints,
+  mockEndpointOnce
+} from "request-registry-mock";
+import { render } from "react-testing-library";
+
+afterAll(() => {
+  unmockAllEndpoints();
+});
+
+describe("request-registry-react", () => {
+  describe("useGetEndPoint", () => {
+    it("renders loading state", () => {
+      const userEndpoint = createGetEndpoint<{ id: string }, { name: string }>({
+        url: ({ id }) => `/user/${id}`
+      });
+      mockEndpoint(userEndpoint, async () => ({ name: "Alex" }));
+      const UserDetails = (props: { id: string }) => {
+        const endpointState = useGetEndPoint(userEndpoint, { id: props.id });
+        if (endpointState.state !== "load") {
+          return <div>loading</div>;
+        }
+        return <div>{endpointState.data.name}</div>;
+      };
+      const { container } = render(<UserDetails id="4" />);
+      expect(container.innerHTML).toEqual("<div>loading</div>");
+    });
+
+    it("renders load data", async () => {
+      const userEndpoint = createGetEndpoint<{ id: string }, { name: string }>({
+        url: ({ id }) => `/user/${id}`
+      });
+      mockEndpoint(userEndpoint, async () => ({ name: "Alex" }));
+      const UserDetails = (props: { id: string }) => {
+        const endpointState = useGetEndPoint(userEndpoint, { id: props.id });
+        if (endpointState.state !== "load") {
+          return <div>loading</div>;
+        }
+        return <div>{endpointState.data.name}</div>;
+      };
+      const { container } = render(<UserDetails id="4" />);
+      await userEndpoint({ id: "4" });
+
+      expect(container.innerHTML).toEqual("<div>Alex</div>");
+    });
+
+    it("rerenders data if cache is invalidated", async () => {
+      const runsEndpoint = createGetEndpoint<{}, { run: number }>({
+        url: () => `/runs`
+      });
+      let runs = 0;
+      mockEndpoint(runsEndpoint, async () => ({ run: runs++ }));
+      const Runs = () => {
+        const endpointState = useGetEndPoint(runsEndpoint, {});
+        if (endpointState.state !== "load") {
+          return <div>loading</div>;
+        }
+        return <div>{endpointState.data.run}</div>;
+      };
+      const { container } = render(<Runs />);
+      await runsEndpoint({});
+      expect(container.innerHTML).toEqual("<div>0</div>");
+      runsEndpoint.clearCache();
+      await runsEndpoint({});
+      expect(container.innerHTML).toEqual("<div>1</div>");
+    });
+
+    it("will take care of a slow outdated request", async () => {
+      const userEndpoint = createGetEndpoint<
+        { id: string },
+        { name: string; age: number }
+      >({
+        url: keys => `/user/${keys.id}`
+      });
+      mockEndpointOnce(
+        userEndpoint,
+        async () => ({ name: "Slow", age: 20 }),
+        20
+      );
+      const UserDetails = (props: { id: string }) => {
+        const endpointState = useGetEndPoint(userEndpoint, { id: props.id });
+        if (endpointState.state !== "load") {
+          return <div>loading</div>;
+        }
+        return <div>{endpointState.data.name}</div>;
+      };
+      const { container } = render(<UserDetails id="4" />);
+      // Execute slow reqeust
+      const slowRequest = userEndpoint({ id: "4" });
+      mockEndpointOnce(userEndpoint, async () => ({ name: "Fast", age: 1 }), 1);
+      userEndpoint.clearCache();
+      // Execute fast request
+      await userEndpoint({ id: "4" });
+      await slowRequest;
+      // Make sure that the slow request which ended later is not shown in the result:
+      expect(container.innerHTML).toEqual("<div>Fast</div>");
+    });
+  });
+});
