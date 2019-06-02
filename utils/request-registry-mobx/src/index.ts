130057916a35962398693624f17fccceddf62a41
Jan Nicklas <j.nicklas@me.com>
2019-06-02 22:07:59 +0200

    fix(request-registry-mobx): Remove synchronous side effect

--- utils/request-registry-mobx/src/index.ts
+++ utils/request-registry-mobx/src/index.ts
@@ -54,14 +54,14 @@ export function createObservableEndpoint<
 		},
 		{
 			/** The observable ajax response result - Will be `undefined` before the first request finished */
-			value: { get: () => mobxEndpoint.state.value, set: noop },
+			value: { get: () => mobxEndpoint.value, set: noop },
 			/** The observable busy state - Will be `true` while an ajax request is running */
 			busy: { get: () => mobxEndpoint.state.busy, set: noop },
 			/** The observable state - Will be `"LOADING" | "UPDATING" | "LOAD" | "ERROR"` */
 			state: { get: () => mobxEndpoint.state.state, set: noop },
-			/** Wether the endpoint has Data` */
+			/** Wether the endpoint has data */
 			hasData: {
-				get: () => mobxEndpoint.state.value !== undefined,
+				get: () => mobxEndpoint.value !== undefined,
 				set: noop,
 			},
 		}
@@ -77,32 +77,36 @@ class MobxEndpoint<
 	endpoint: TEndpoint;
 	/** Request parameters */
 	keys?: TKeys;
-	/** Holds all cleanup functions which should be called 
+	/** Holds all cleanup functions which should be called
       once this class becomes unobserved */
 	disposables = [] as Array<() => any>;
 	/** The cacheKey for the latest keys to execute the endpoint
       only if the request parameters have changed */
 	_cacheKey?: string;
 	/** The ajax result */
-	_value?: TResult;
+	value?: TResult;
 	/** The ajax error */
 	_error?: any;
 	/** The ajax promise */
 	_valuePromise?: Promise<any>;
-	/** Will be true as long as an ajax request is running */
-	busy: boolean = false;
+	/**
+	 *  Will be true as long as an ajax request is running
+	 *  or if before the first ajax request has started
+	 */
+	busy: boolean = true;
 
 	constructor(endpoint: TEndpoint, keys?: TKeys) {
 		this.keys = keys;
 		this.endpoint = endpoint;
-		// Track the observe state of the computed this.state
+		// Track the observe state of the observable this.value
+		// which is used by the computed this.state
 		// to manage the endpoint keepInCache state
 		// and to refresh the endpoint in case of a
 		// cache invalidation
-		onBecomeObserved(this, 'state', () => {
+		onBecomeObserved(this, 'value', () => {
 			this.watchEndpoint();
 		});
-		onBecomeUnobserved(this, 'state', () => {
+		onBecomeUnobserved(this, 'value', () => {
 			this.dispose();
 		});
 	}
@@ -114,6 +118,9 @@ class MobxEndpoint<
 
 	/** Execute the endpoint again if the cache of the endpoint is invalidated */
 	watchEndpoint() {
+		if (!this.keys) {
+			return;
+		}
 		// Make sure that the garbage collector does not clean up the endpoint result:
 		this.disposables.push(this.endpoint.keepInCache(this.keys));
 		// If the cache is cleared execute the endpoint again
@@ -122,7 +129,6 @@ class MobxEndpoint<
 				if (this.keys === undefined) {
 					return;
 				}
-				this.busy = true;
 				return this.executeEndpoint();
 			})
 		);
@@ -135,7 +141,7 @@ class MobxEndpoint<
 
 	/** Handle ajax response */
 	receiveData(result: TResult) {
-		this._value = result as TResult;
+		this.value = result as TResult;
 		this.busy = false;
 	}
 
@@ -151,14 +157,16 @@ class MobxEndpoint<
 			: undefined;
 		if (currentCacheKey && this._cacheKey !== currentCacheKey) {
 			this._cacheKey = currentCacheKey;
-			this.busy = true;
 			this.dispose();
 			this.watchEndpoint();
+			// Let mobx finish its current processing to ensure
+			// that the busy state is not changed syncronously
+			// from within a computed function
 			this.executeEndpoint();
 		}
 		// Return value from cache
 		const busy = this.busy;
-		const value = this._value;
+		const value = this.value;
 		const error = this._error;
 		const hasData = value !== undefined;
 		if (error && !busy) {
@@ -199,22 +207,40 @@ class MobxEndpoint<
 	}
 
 	executeEndpoint() {
+		let currentPromiseDone = false;
 		// Store the ajax promise to track if this is the latest promise
 		const currentPromise = (this._valuePromise = this.endpoint(
 			this.keys
 		).then(result => {
+			currentPromiseDone = true;
 			// If the keys are not up to date anymore ignore the result
 			if (currentPromise === this._valuePromise) {
 				this.receiveData(result);
 			}
 		}));
 		currentPromise.catch(error => {
+			currentPromiseDone = true;
 			// If the keys are not up to date anymore ignore the result
 			if (currentPromise === this._valuePromise) {
 				this.receiveError(error);
 			}
 			return currentPromise;
 		});
+		// Writing to `this.busy` might happen as a side effect of
+		// getState - as this must not happen synchronously wrap it
+		// with a set timeout
+		setTimeout(
+			action(() => {
+				// Ensure that this is still the correct promise
+				// and that it has not been finished in the meantime
+				if (
+					!currentPromiseDone &&
+					currentPromise === this._valuePromise
+				) {
+					this.busy = true;
+				}
+			})
+		);
 		return currentPromise;
 	}
 }
@@ -224,7 +250,7 @@ decorate(MobxEndpoint, {
 	receiveError: action,
 	keys: observable,
 	busy: observable,
-	_value: observable,
+	value: observable,
 	_error: observable,
 	setKeys: action,
 	state: computed,
